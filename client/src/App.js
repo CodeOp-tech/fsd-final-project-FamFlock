6b1d4e54aceb058b845e26c67b8fbde83ff0b368
Maria Quiles <mariaquiles@MacBook-Air-de-Maria.local>
2022-07-25 10:23:00 +0200

    trips route, app update, MyTripsView created on front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import React, {  } from "react";
+// import {  } from "react-router-dom";
+import "./App.css";
+
+// import views
+// import MyTripsView from "./views/MyTripsView";
 
 function App() {
   return (
---
+++ client/src/views/TripsView
@@ -0,0 +1,5 @@
+// import React, {  } from "react";
+
+function MyTripsView(props) {}
+
+export default MyTripsView;
