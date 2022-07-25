f7d578c53dc930cfeca734459f89574945270193
Fabiana Quiles <heyheyfabi@gmail.com>
2022-07-25 12:27:06 +0200

    TripsView map function to row 1 of trips

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import NavBar from "./components/NavBar";
 import HomeView from "./views/HomeView";
+import TripsView from "./views/TripsView";
 
 function App() {
+  let [trips, setTrips] = useState([]); // don't forget num of state
+
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<HomeView />} />
+        <Route path="/my-trips" element={<TripsView trips={trips} />} />
       </Routes>
     </div>
   );
--- client/src/views/TripsView.js
+++ client/src/views/TripsView.js
@@ -1,5 +1,32 @@
-// import React, {  } from "react";
+import React, { useState, useEffect } from "react";
 
-function MyTripsView(props) {}
+function TripsView(props) {
+  const trips = props.trips;
 
-export default MyTripsView;
+  const [featTrip, setFeatTrip] = useState(null);
+
+  useEffect(() => {
+    setFeatTrip(trips[0]);
+  }, [props.trips]);
+
+  return (
+    <div>
+      <h4>This is the Trips view</h4>
+      {/* trips grid */}
+      <div className="row">
+        {trips.map((trip) => (
+          <div className="card col-md-3 p-4">
+            <h4 className="card-title">title{trip.destination}</h4>
+            {/*{trips.destination}*/}
+            <h6 className="card-subtitle">subtitle{trip.startDate}</h6>
+            {/*{trips.startDate}*/}
+            <p className="card-text">text{trip.endDate}</p>
+            {/*{trips.endDate}*/}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TripsView;
