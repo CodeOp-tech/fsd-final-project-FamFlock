e770901dcad059088dcaaf18a55ebda17ed5228b
Miselta Ihekwoaba <miselta@Miseltas-MacBook-Pro.local>
2022-07-25 14:38:51 +0200

    working on login FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -7,14 +7,46 @@ import HomeView from "./views/HomeView";
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
 import MyProfileView from "./views/MyProfileView";
+import Local from "./helpers/Local";
+import Api from "./helpers/Api";
 
 function App() {
+  const [user, setUser] = useState(Local.getUser()); // useState 1: sets logged in user
+  const [loginErrorMessage, setLoginErrorMessage] = useState(""); // useState 2
+
+  // log in
+  async function doLogin(username, password) {
+    let myresponse = await Api.loginUser(username, password);
+    if (myresponse.ok) {
+      Local.saveUserInfo(myresponse.data.token, myresponse.data.user);
+      setUser(myresponse.data.user);
+      setLoginErrorMessage("");
+    } else {
+      setLoginErrorMessage("Login failed");
+    }
+  }
+
+  // log out
+  function doLogout() {
+    Local.removeUserInfo();
+    setUser(null);
+  }
+
+  // register
+
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<HomeView />} />
-        <Route path="/login" element={<LoginView />} />
+        <Route
+          path="/login"
+          element={
+            <LoginView
+              loginCb={(username, password) => doLogin(username, password)}
+            />
+          }
+        />
         <Route path="/register" element={<RegisterView />} />
         <Route path="/profile/:id" element={<MyProfileView />} />
       </Routes>
