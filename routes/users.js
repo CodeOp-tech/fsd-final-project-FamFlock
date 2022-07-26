3a4a7cf404aef96d5274327e2adf0147fa5fb1bc
Pia Nichelle prozesky <pianichelleprozesky@Pias-MacBook-Air.local>
2022-07-26 11:04:15 +0200

    edit profile started

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
 
 /* GET users */
 router.get("/", async function (req, res, next) {
@@ -21,6 +24,24 @@ router.get("/:id", async function (req, res, next) {
     }
   } catch (err) {
     res.status(500).send({ error: err.message });
+  }
+});
+
+//For now Im only letting the user update its email and password
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { picture, fullname, email, username, password } = req.body;
+
+    let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    console.log(hashedPassword);
+    let sql = `
+              UPDATE users SET picture = '${picture}', fullname = '${fullname}', email = '${email}', username = '${username}', password = '${hashedPassword}' WHERE id = ${id}`;
+    await db(sql);
+    res.status(200).send({ message: "Information updated successfully" });
+  } catch (error) {
+    res.send({ message: error });
   }
 });
 
