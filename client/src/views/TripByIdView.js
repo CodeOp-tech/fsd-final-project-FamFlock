49ad310a997065fe4f1adefd0ada789a44e79496
Pia Nichelle prozesky <pianichelleprozesky@Pias-MacBook-Air.local>
2022-08-02 14:38:48 +0200

    vertical navbar started

--- client/src/views/TripByIdView.js
+++ client/src/views/TripByIdView.js
@@ -6,6 +6,7 @@ import MapsView from "./MapsView";
 import UserContext from "../context/UserContext";
 import TripsContext from "../context/TripsContext";
 import { useEffect } from "react";
+import TripByIdCss from "./TripByIdView.css";
 
 function TripByIdView(props) {
   const { trip, goToMapsView, getTrip, fetchItineraries } =
@@ -23,18 +24,23 @@ function TripByIdView(props) {
 
   return (
     <div>
-      <h2>Basic info</h2>
-      <h2>Members</h2>
-      <h2>Chat</h2>
-      <nav>
-        <Link to={`/chat/${props.groupId}`}> View the chat here</Link>
-      </nav>
-      <h2>Itinerary</h2>
-      <Link to={"/itinerary"}>Take a look at your itinerary!</Link>
-      <h2>Lists</h2>
-      <Link to={"/lists"}>Use your lists to get ready</Link>
-      <h2>Map</h2>
-      <button onClick={() => goToMapsView(trip.id)}>Go to Maps</button>
+      <ul className="verticalNav">
+        <li className="vertNavLi">Basic info</li>
+        <li className="vertNavLi">Members</li>
+        <li className="vertNavLi">
+          <Link to={`/chat/${props.groupId}`}>Chat</Link>
+        </li>
+        <li className="vertNavLi">
+          <Link to={"/itinerary"}>Itinerary</Link>
+        </li>
+        <li className="vertNavLi">
+          <Link to={"/lists"}>Lists</Link>
+        </li>
+        <li className="vertNavLi">
+          Map
+          <button onClick={() => goToMapsView(trip.id)}>Go to Maps</button>
+        </li>
+      </ul>
     </div>
   );
 }
