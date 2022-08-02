fe8887ac6aa3f4d055848f6baeebc50a67c2ba25
Pia Nichelle prozesky <pianichelleprozesky@Pias-MacBook-Air.local>
2022-08-02 16:05:35 +0200

    vertical navbar for trip by id moved to componets

--- client/src/components/TripByIdNav.js
+++ client/src/components/TripByIdNav.js
@@ -1,12 +1,12 @@
 import React, { useState, useContext } from "react";
 
-import TripList from "../components/TripList";
+import TripList from "./TripList";
 import { Route, Routes, Link, Navigate } from "react-router-dom";
-import MapsView from "./MapsView";
+import MapsView from "../views/MapsView";
 import UserContext from "../context/UserContext";
 import TripsContext from "../context/TripsContext";
 import { useEffect } from "react";
-import TripByIdCss from "./TripByIdView.css";
+import TripByIdNavCss from "../components/TripByIdNav.css";
 
 function TripByIdView(props) {
   const {
@@ -16,20 +16,26 @@ function TripByIdView(props) {
     getTrip,
     fetchItineraries,
     goToChatView,
+    goToListView,
+    goToMembersView,
   } = useContext(TripsContext);
 
   return (
     <div>
       <ul className="verticalNav">
         <li className="vertNavLi">Basic info</li>
-        <li className="vertNavLi">Members</li>
+        <li className="vertNavLi" onClick={(e) => goToMembersView(trip.id)}>
+          Members
+        </li>
         <li className="vertNavLi" onClick={(e) => goToChatView(trip.id)}>
           Chat
         </li>
         <li className="vertNavLi" onClick={(e) => goToItineraryView(trip.id)}>
           Itinerary
         </li>
-        <li className="vertNavLi">Lists</li>
+        <li className="vertNavLi" onClick={(e) => goToListView(trip.id)}>
+          Lists
+        </li>
         <li className="vertNavLi" onClick={() => goToMapsView(trip.id)}>
           Map
         </li>
