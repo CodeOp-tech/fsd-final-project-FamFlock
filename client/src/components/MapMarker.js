bac0c08bdee34f0aab88bc6c86aa034e966526d3
Maria-Llaverias <llaverias.maria@gmail.com>
2022-08-03 10:32:47 +0200

    same commit with comments again

--- client/src/components/MapMarker.js
+++ client/src/components/MapMarker.js
@@ -69,7 +69,7 @@ function MarkerMap(props) {
             <Marker
               key={p.latLng}
               position={p.latLng.split(",")}
-              // im splitting the position because the database is saving it as a string and the component its expecting an array.
+              // im splitting the position because the database is saving it as a string and the component git its expecting an array.
               icon={redMarker}
             >
               <Popup>{p.name}</Popup>
